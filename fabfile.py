1296e739a130c346a0575033abfd72b2273fa0f0
adam schweigert <aschweigert@gmail.com>
2014-06-13 11:57:21 -0400

    add environment variables

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from tools.fablib import *
 """
 Base configuration
 """
-env.project_name = ''
+env.project_name = 'wcij'
 env.file_path = '.'
 
 # Environments
@@ -12,19 +12,18 @@ def production():
     Work on production environment
     """
     env.settings = 'production'
-    env.hosts = []
-    env.user = ''
-    env.password = ''
-
+    env.hosts = [os.environ['WCIJ_PRODUCTION_SFTP_HOST'], ]
+    env.user = os.environ['WCIJ_PRODUCTION_SFTP_USER']
+    env.password = os.environ['WCIJ_PRODUCTION_SFTP_PASSWORD']
 
 def staging():
     """
     Work on staging environment
     """
     env.settings = 'staging'
-    env.hosts = []
-    env.user = ''
-    env.password = ''
+    env.hosts = [os.environ['WCIJ_STAGING_SFTP_HOST'], ]
+    env.user = os.environ['WCIJ_STAGING_SFTP_USER']
+    env.password = os.environ['WCIJ_STAGING_SFTP_PASSWORD']
 
 try:
     from local_fabfile import  *
