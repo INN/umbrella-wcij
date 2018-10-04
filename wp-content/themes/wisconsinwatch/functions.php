7a6d547ee34770bec1cdb02cbb62d2c07fb4b8fa
tylermachado <tyler@inn.org>
2018-10-04 15:04:27 -0400

    automatic date-time string bumping

--- wp-content/themes/wisconsinwatch/functions.php
+++ wp-content/themes/wisconsinwatch/functions.php
@@ -38,7 +38,7 @@ function child_stylesheet() {
 		'wcij',
 		get_stylesheet_directory_uri() . '/css/child' . $suffix . '.css',
 		null,
-		'2018-10-04'
+		filemtime( get_stylesheet_directory() . '/css/child.css' )
 	);
 }
 add_action( 'wp_enqueue_scripts', 'child_stylesheet', 20 );
